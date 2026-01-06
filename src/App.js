dbe1056771299b0395c7e2659b8eb882b1c1b029
Ben Neuendorf <benneuendorf@Mini-von-Ben.fritz.box>
2026-01-06 17:02:58 +0100

    Update App.js with auth routes

--- src/App.js
+++ src/App.js
@@ -1,4 +1,7 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
+import { AuthProvider } from './contexts/AuthContext'
+import ProtectedRoute from './components/ProtectedRoute'
+import LoginPage from './pages/LoginPage'
 import StartPage from './pages/StartPage'
 import ProtocolPage from './pages/ProtocolPage'
 import AdminPage from './pages/AdminPage'
@@ -7,13 +10,43 @@ import RentalFormPage from './pages/RentalFormPage'
 function App() {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<StartPage />} />
-        <Route path="/protocol/:rentalId/:type" element={<ProtocolPage />} />
-        <Route path="/admin" element={<AdminPage />} />
-        <Route path="/admin/new" element={<RentalFormPage />} />
-        <Route path="/admin/edit/:id" element={<RentalFormPage />} />
-      </Routes>
+      <AuthProvider>
+        <Routes>
+          <Route path="/login" element={<LoginPage />} />
+          
+          <Route path="/" element={
+            <ProtectedRoute>
+              <StartPage />
+            </ProtectedRoute>
+          } />
+          
+          <Route path="/protocol/:rentalId/:type" element={
+            <ProtectedRoute>
+              <ProtocolPage />
+            </ProtectedRoute>
+          } />
+          
+          <Route path="/admin" element={
+            <ProtectedRoute>
+              <AdminPage />
+            </ProtectedRoute>
+          } />
+          
+          <Route path="/admin/new" element={
+            <ProtectedRoute>
+              <RentalFormPage />
+            </ProtectedRoute>
+          } />
+          
+          <Route path="/admin/edit/:id" element={
+            <ProtectedRoute>
+              <RentalFormPage />
+            </ProtectedRoute>
+          } />
+
+          <Route path="*" element={<Navigate to="/" replace />} />
+        </Routes>
+      </AuthProvider>
     </BrowserRouter>
   )
 }
